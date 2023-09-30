335165827b9dabdaf38c26702464bd111364b049
Manh Le <manhlq91@gmail.com>
2023-10-01 00:38:18 +0700

    Ack when process message

--- src/consumer.js
+++ src/consumer.js
@@ -1,32 +1,30 @@
 const amqp = require('amqplib/callback_api');
 var config = require('./config')
 
-
 var args = process.argv.slice(1);
 
-let queueUri = config['sourceUri']
-console.log(args)
-if (args[1] == 'new') { // switch to new cluster
-    queueUri = config['destUri']
-}
+const switched = args[1] == 'new' ? true : false
+let queueUri = switched ? config['destUri'] : config['sourceUri']
 
-console.log(config)
-console.log(queueUri);
+const exchange = switched ? config['destExchange'] : config['sourceExchange'];
+const routingKey = config['routingKey'];
+const queueName = switched ? config['destQueue'] : config['sourceQueue']
+
 amqp.connect(queueUri, (error0, connection) => {
     if (error0) {
         throw error0;
     }
+    console.log(`Subscribing ${switched ? 'new' : ''} cluster at ${queueUri}`);
     connection.createConfirmChannel((error1, channel) => {
         if (error1) {
             throw error1;
         }
-        var exchange = config['sourceExchange'];
 
         channel.assertExchange(exchange, 'topic', {
             durable: true
         });
 
-        channel.assertQueue(config['sourceQueue'],
+        channel.assertQueue(queueName,
             {
                 autoDelete: false,
                 durable: true,
@@ -34,12 +32,12 @@ amqp.connect(queueUri, (error0, connection) => {
                 if (error2) {
                     throw error2;
                 }
-                console.log(' [*] Waiting for messages. To exit press CTRL+C');
-                channel.bindQueue(q.queue, exchange, config['routingKey']);
+                console.log(` [*] Waiting for messages on exchange ${exchange} on routing key ${routingKey}. To exit press CTRL+C`);
+                channel.bindQueue(q.queue, exchange, routingKey);
                 channel.consume(q.queue, msg => {
                     console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
                 }, {
-                    noAck: false,
+                    noAck: true,
                 })
             });
     });
--- src/publisher.js
+++ src/publisher.js
@@ -6,21 +6,21 @@ var args = process.argv.slice(1);
 const switched = args[1] == 'new' ? true : false
 let queueUri = switched ? config['destUri'] : config['sourceUri']
 
-console.log(config)
-console.log(queueUri);
-
 const exchange = switched ? config['destExchange'] : config['sourceExchange'];
 const routingKey = config['routingKey'];
+const MAX_MSG = 100;
 
-amqp.connect((err, connection) => {
+amqp.connect(queueUri, (err, connection) => {
+    console.log(`Connected to ${switched ? 'new' : 'current'} cluster at ${queueUri}`)
     if (err) return bail(err);
     connection.createChannel((err, channel) => {
         if (err) return bail(err, connection);
         channel.assertExchange(exchange, 'topic', { durable: true }, (err) => {
             if (err) return bail(err, connection);
+            console.log(`Publishing message to exchange ${exchange} on routing key ${routingKey}`)
             let counter = 0;
-            while (counter < 5000) {
-                const text = `Order-${counter + 1}`
+            while (counter < MAX_MSG) {
+                const text = `Order-${counter + 1 + (switched ? MAX_MSG : 0)}`
                 channel.publish(exchange, routingKey, Buffer.from(text));
                 console.log(" [x] Sent '%s'", text);
                 counter += 1;
